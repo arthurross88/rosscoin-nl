6243378dea52aa529235d820704dd489578a5c38
Arthur Ross <arthurross@live.nl>
2022-06-05 19:55:16 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,26 +116,6 @@ const IndexPage = () => (
         {i !== moreLinks.length - 1 && <> · </>}
       </React.Fragment>
     ))}
-    <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCzJK-GAICoPTZwHwSBiPr5thMP3hHm56s",
-    authDomain: "magicmirror-9f36e.firebaseapp.com",
-    databaseURL: "https://magicmirror-9f36e.firebaseio.com",
-    projectId: "magicmirror-9f36e",
-    storageBucket: "magicmirror-9f36e.appspot.com",
-    messagingSenderId: "94780094094",
-    appId: "1:94780094094:web:6b16e221cbbed526057bdf"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
   </Layout>
 )
 
