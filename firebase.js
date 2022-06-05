1787f9c1febfaa10927afc259c83b73e568f8f18
Arthur Ross <arthurross@live.nl>
2022-06-05 20:07:16 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzJK-GAICoPTZwHwSBiPr5thMP3hHm56s",
+  authDomain: "magicmirror-9f36e.firebaseapp.com",
+  databaseURL: "https://magicmirror-9f36e.firebaseio.com",
+  projectId: "magicmirror-9f36e",
+  storageBucket: "magicmirror-9f36e.appspot.com",
+  messagingSenderId: "94780094094",
+  appId: "1:94780094094:web:6b16e221cbbed526057bdf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
