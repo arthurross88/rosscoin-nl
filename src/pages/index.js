454f061e287270f167dfc29803ba8b77e1d2bc18
Arthur Ross <arthurross@live.nl>
2022-06-05 19:51:32 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCzJK-GAICoPTZwHwSBiPr5thMP3hHm56s",
-  authDomain: "magicmirror-9f36e.firebaseapp.com",
-  databaseURL: "https://magicmirror-9f36e.firebaseio.com",
-  projectId: "magicmirror-9f36e",
-  storageBucket: "magicmirror-9f36e.appspot.com",
-  messagingSenderId: "94780094094",
-  appId: "1:94780094094:web:6b16e221cbbed526057bdf"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const links = [
   {
     text: "Tutorial",
@@ -137,3 +120,23 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCzJK-GAICoPTZwHwSBiPr5thMP3hHm56s",
+    authDomain: "magicmirror-9f36e.firebaseapp.com",
+    databaseURL: "https://magicmirror-9f36e.firebaseio.com",
+    projectId: "magicmirror-9f36e",
+    storageBucket: "magicmirror-9f36e.appspot.com",
+    messagingSenderId: "94780094094",
+    appId: "1:94780094094:web:6b16e221cbbed526057bdf"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
