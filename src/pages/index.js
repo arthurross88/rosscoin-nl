2f7f5277edc10ea416c433241d6775397d0decd7
Arthur Ross <arthurross@live.nl>
2022-06-05 14:37:27 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,13 +83,6 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-          <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P264FQB');</script>
-<!-- End Google Tag Manager -->
         Welcome to <b>Gatsby!</b>
       </h1>
       <p className={styles.intro}>
